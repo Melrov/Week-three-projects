f879aa272981cd146702f18a847b53ffcee85785
Melrov <rdunham918@gmail.com>
2022-05-08 23:31:11 -0500

    formating

--- BlackJack/listeners.js
+++ BlackJack/listeners.js
@@ -1,30 +1,30 @@
-let playerJoinDivs = []
-let playerLockBetDivs = []
+let playerJoinDivs = [];
+let playerLockBetDivs = [];
 for (let i = 1; i <= 5; i++) {
-    playerJoinDivs.push(document.getElementById(`p${i}-join`))
-    playerLockBetDivs.push(document.getElementById(`p${i}-lockBet`))
+  playerJoinDivs.push(document.getElementById(`p${i}-join`));
+  playerLockBetDivs.push(document.getElementById(`p${i}-lockBet`));
 }
 
-for(let i = 0; i < playerJoinDivs.length; i++){
-    playerJoinDivs[i].addEventListener('click', current.playerJoin)
-    playerLockBetDivs[i].addEventListener('click', current.betLock)
+for (let i = 0; i < playerJoinDivs.length; i++) {
+  playerJoinDivs[i].addEventListener("click", current.playerJoin);
+  playerLockBetDivs[i].addEventListener("click", current.betLock);
 }
 
-document.querySelectorAll('.chip').forEach(chip => {
-    chip.addEventListener('click', current.addBet)
-})
+document.querySelectorAll(".chip").forEach((chip) => {
+  chip.addEventListener("click", current.addBet);
+});
 
-let hit = document.getElementById('hit')
-hit.addEventListener('click', current.hit)
+let hit = document.getElementById("hit");
+hit.addEventListener("click", current.hit);
 
-let stand = document.getElementById('stand')
-stand.addEventListener('click', current.stand)
+let stand = document.getElementById("stand");
+stand.addEventListener("click", current.stand);
 
-let split = document.getElementById('split')
-split.addEventListener('click', current.split)
+let split = document.getElementById("split");
+split.addEventListener("click", current.split);
 
-let double = document.getElementById('double')
-double.addEventListener('click', current.double)
+let double = document.getElementById("double");
+double.addEventListener("click", current.double);
 
-let surrender = document.getElementById('surrender')
-surrender.addEventListener('click', current.surrender)
+let surrender = document.getElementById("surrender");
+surrender.addEventListener("click", current.surrender);
